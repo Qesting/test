a0c189b6a73d89c04e8527528598ca7775db918c
Krzysztof Kulig <wortot.pl@gmail.com>
2022-10-18 19:28:45 +0200

    Aktualizacja strony głównej (#4)

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-    session_start();
-    
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    
-    require_once('config/config.php');
-
-    if (isset($_SESSION['question'])) {
-        require_once("tests/unset.php");
-    }
 
     if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['test'])) {
 
@@ -20,43 +9,42 @@
 
     }
 
-    $link = dbConnect();
 ?>
 
 <!DOCTYPE html>
-<html>
+<html leng='pl-Pl'>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Indeks</title>
         <link rel="stylesheet" href="style/main.css">
         <style>
-            ul {
-                list-style-type: none;
+            body{
+                font: 14px sans-serif;
+                margin-bottom: 120px;
             }
-            body{ font: 14px sans-serif; text-align: center;}
+            .row { margin-top: -20px; }
+            .row > * { margin-top: 20px; }
         </style>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
     </head>
-    <body class="bootstrap-dark">
+    <body>
         <nav class="navbar navbar-expand navbar-dark bg-primary">
             <div class="container-fluid">
                 <a class="navbar-brand"><b>T</b>ESTOPOL</a>
                 <div class="collapse navbar-collapse">
                     <div class="navbar-nav ms-auto">
-                        <?php
-                            if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-                                echo "<a class='nav-item nav-link active' href='ui/login.php'>Zaloguj się</a>
-                                <a class='nav-item nav-link active' href='ui/register.php'>Zarejestruj się</a>";
-                            } else {
-                                echo "<span class='nav-item nav-link active'>Witaj, <b>${_SESSION['username']}</b></span>
-                                <a class='nav-item nav-link active' href='ui/userpage.php'><u>Strona użytkownika</u></u></a>";
-                            }
-                        ?>
+                        <a class='nav-item nav-link active' href='..'>Do strony głównej</a>
+                        <?php if(!isset($_SESSION["loggedin"]) || !$_SESSION["loggedin"]): ?>
+                            <a class='nav-item nav-link active' href='ui/login.php'>Zaloguj się</a>
+                            <a class='nav-item nav-link active' href='ui/register.php'>Zarejestruj się</a>
+                        <?php else: ?>
+                            <span class='nav-item nav-link active'>Witaj, <b><?php echo $_SESSION['username'] ?></b></span>
+                            <a class='nav-item nav-link active' href='ui/userpage.php'><u>Strona użytkownika</u></a>
+                        <?php endif; ?>
                     </div>
                 </div>
                 <div class="text-right pl-3"></div>
@@ -64,49 +52,45 @@
             </div>
         </nav>
         <div class="wrapper">
-            <h2 class="my-5">TESTOPOL</h2>
-            <p>Witamy w systemie testów TESTOPOL.</p>
-            <p>Wybierz test</p>
-            <div class="container-fluid">
-            <?php
-                $res1 = $link->query("SELECT * FROM module");
-
-                $sql = $link->prepare("SELECT id, name FROM test WHERE module_id=?");
-
-                while ($row1 = $res1->fetch_assoc()) {
-                    if (($row1['id']%3) == 1) {
-                        echo '<div class="row">';
-                    }
-                    echo '<div class="col-md-4 mb-3">';
-                    echo '<button class="btn btn-primary btn-lg btn-block" data-toggle="collapse" data-target="#mod'.$row1['id'].'" aria-expanded="false" aria-controls="collapseExample">'.$row1['name'].'</button>';
-
-                    $mid = $row1['id'];
-                    $sql->bind_param('i', $mid);
-                    $sql->execute();
-                    $res2 = $sql->get_result();
-
-                    echo '<ul id="mod'.$row1['id'].'" class="collapse card card-body">';
-                    while ($row2 = $res2->fetch_assoc()) {
-                        echo "<li class=\"card card-body mb-2\">
-                        <p>${row2['name']}</p>
-                            <a href='tests/start.php?t=${row2['id']}' class='btn btn-secondary btn-sm'>Start</a>
-                        </li>";
-                    }
-
-                    echo "</ul></div>";
-
-                    if (($row1['id']%3) == 0) {
-                        echo '</div>';
-                    }
-                }
-                echo '<div class="row"></div>';
-
-                $sql->close();
-                $link->close();
-            ?>
+            <div class='container my-3'>
+                <div class='text-center'>
+                    <h1 class='text-uppercase'>Testopol</h1>
+                    <p>Testopol to hobbystycznie stworzony system dla testów, który zrodził się z potrzeby znalezienia zamiennika dla innego, dużo bardziej popularnego serwisu podobnego typu.</p>
+                    <p>Tworzony jest od kwietnia 2022 i od tego czasu przeszedł już kilka etapów, za każdym razem zwiększając ilość i jakość swoich funkcjonalności, a także poziom bezpieczeństwa. </p>
+                </div>
             </div>
-            
+            <div class='container text-center text-white'>
+                <div class='row'>
+                    <div class='col-md-6 p-3 bg-primary' id='container-test'>
+                        <h4 class='mb-4'><span class='bi-card-text'></span> Testy</h4>
+                        <div>
+                            <p>Główna część systemu Testopol. System posiada wszystkie funkcjonalności potrzebne do tworzenia prostych testów sprawdzających, a także mechanizmy blokowania testu, sesji i udostępniania odpowiedzi po rozwiązaniu.</p>
+                            <a class='btn btn-secondary' href='tests'>Zobacz listę testów</a>
+                        </div>
+                        <div class='mt-5'>
+                            <p>Nie udało ci się znaleźć odpowiedniego testu? <u><a class='text-white text-underline' href='ui/userpage.php'>Stwórz własny.</a></u></p>
+                        </div>
+                    </div>
+                    <div class='col-md-6 p-3 bg-secondary' id='container-article'>
+                        <h4 class='mb-4'>Materiały <span class='bi-journal-text'></span></h4>
+                        <div>
+                            <p>Materiały do nauki, czyli system tworzenia i przeglądania artykułów. Możliwość oznaczenia artykułu jako materiał powtórzeniowy dla konkretnego testu. Formatowanie oparte jest na języku Markdown.</p>
+                            <a class='btn btn-primary' href='article'>Zobacz materiały</a>
+                        </div>
+                        <div class='mt-5'>
+                            <p>Nie udało ci się znaleźć odpowiednich materiałów? <u><a class='text-white text-underline' href='ui/userpage.php'>Stwórz własne.</a></u></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-         <script src='js/quote.js'></script>
+        <footer class='bg-primary text-light text-center fixed-bottom'>
+            <div class='container p-4 pb-0'>
+                <div class='mb-3'>
+                    <p>Trafiłeś/aś na jakiś błąd w działaniu aplikacji? <u><a class='text-light' href='https://github.com/Qesting/test/issues' target='_blank'>Skontaktuj się z nami</a></u></p>
+                </div>
+            </div>
+        </footer> 
+        <script src='js/quote.js'></script>
     </body>
 </html>
